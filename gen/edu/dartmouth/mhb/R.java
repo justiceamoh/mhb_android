8d0cec5f8d8c80554ce325b1f710af122556ed52
Justice Amoh <justiceamoh@gmail.com>
2013-08-16 12:07:15 +0000

    attempting slide page effects

--- gen/edu/dartmouth/mhb/R.java
+++ gen/edu/dartmouth/mhb/R.java
@@ -48,6 +48,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int searchactivity=0x7f030001;
+        public static final int slidepage_view=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
