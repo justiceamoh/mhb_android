b8fe5a26bd0229546b453b83228e71430721819e
Justice Amoh <justiceamoh@gmail.com>
2013-08-18 11:18:34 +0000

    realtime search loading layout after each word

--- src/edu/dartmouth/mhb/SlidePageActivity.java
+++ src/edu/dartmouth/mhb/SlidePageActivity.java
@@ -7,6 +7,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentActivity;
@@ -17,117 +18,127 @@ import android.view.MenuInflater;
 import android.widget.SearchView;
 
 public class SlidePageActivity extends FragmentActivity {
-	
+	Context context;
 	MySQLiteHelper myDBHelper;
 	private HymnsDataSource datasource;
 	private List<Hymn> hymns;
-	
-    private ViewPager mPager;
-    private PagerAdapter mPagerAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_slide_page);
-        
-        datasource = new HymnsDataSource(this);
-        datasource.open();        
-        hymns = datasource.getAllHymns();
-        
-        
-        List<Fragment> fragments = getFragments();               
-        // Instantiate a ViewPager and a PagerAdapter.
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPagerAdapter = new SlidePageAdapter(getFragmentManager(), fragments);
-        mPager.setAdapter(mPagerAdapter);
+	private ViewPager mPager;
+	private PagerAdapter mPagerAdapter;
 
-               
-        //Reset action bar for each page
-//        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-//            @Override
-//            public void onPageSelected(int position) {
-//                invalidateOptionsMenu();
-//            }
-//        });        
-        
-
-    }
-    @Override
-    protected void onResume(){
-    	datasource.open();
-    	super.onResume();
-    }
-    
-    @Override
-    protected void onPause() {
-    	datasource.close();
-    	super.onPause();
-    }
-       
-    
-    private List<Fragment> getFragments(){
-    	List<Fragment> fList = new ArrayList<Fragment>();
-    	  for(int i=0; i<5; i++){
-    		   	fList.add(SlidePageFragment.newInstance(hymns.get(i)));
-
-          }
-     	    	
-    	return fList;
-    }
-    
-// TODO: menu options and next/previous actions    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_slide_page);
 		
-	    // Get the SearchView and set the searchable configuration
-	    SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
-	    SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-	    searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+		context = getApplicationContext();
+		datasource = new HymnsDataSource(this);
+		datasource.open();
+		hymns = datasource.getAllHymns();
 
-	    // Do not iconify the widget;expand it by default
-	    searchView.setIconifiedByDefault(false);
+		List<Fragment> fragments = getFragments();
+		// Instantiate a ViewPager and a PagerAdapter.
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPagerAdapter = new SlidePageAdapter(getFragmentManager(), fragments);
+		mPager.setAdapter(mPagerAdapter);
 
-    
-        return super.onCreateOptionsMenu(menu);
-    }  
+	}
 
-           
-    @Override
-    public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
-    }
+	@Override
+	protected void onResume() {
+		datasource.open();
+		super.onResume();
+	}
 
+	@Override
+	protected void onPause() {
+		datasource.close();
+		super.onPause();
+	}
 
-    
-    
-    
-    private class SlidePageAdapter extends FragmentPagerAdapter {
-        private List<Fragment> fragments;
-    	
-    	public SlidePageAdapter(FragmentManager fm, List<Fragment> fragments) {
-            super(fm);
-            this.fragments = fragments;
-        }
+	private List<Fragment> getFragments() {
+		List<Fragment> fList = new ArrayList<Fragment>();
+		for (int i = 0; i < 5; i++) {
+			fList.add(SlidePageFragment.newInstance(hymns.get(i)));
 
-        @Override
-        public Fragment getItem(int position) {
-            return this.fragments.get(position);
-        }
-        
-        @Override
-        public int getCount() {
-            return this.fragments.size();
-        }
-    }
+		}
+
+		return fList;
+	}
+
+	// TODO: menu options and next/previous actions
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+
+		// Get the SearchView and set the searchable configuration
+		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		SearchView searchView = (SearchView) menu.findItem(R.id.menu_search)
+				.getActionView();
+		searchView.setSearchableInfo(searchManager
+				.getSearchableInfo(getComponentName()));
+
+		// Do not iconify the widget;expand it by default
+		searchView.setIconifiedByDefault(false);
+		
+		
+		
+		final SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+		    @Override
+		    public boolean onQueryTextChange(String newText) {
+		        // Do something
+		    	
+		    	Intent i = new Intent(context, SearchActivity.class);
+		    	i.putExtra(SearchManager.QUERY, newText);
+		    	startActivity(i); 		    	
+		        return true;
+		    }
+
+		    @Override
+		    public boolean onQueryTextSubmit(String query) {
+		        // Do something
+		        return true;
+		    }
+		};
+
+		searchView.setOnQueryTextListener(queryTextListener);		
+		
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (mPager.getCurrentItem() == 0) {
+			// If the user is currently looking at the first step, allow the
+			// system to handle the
+			// Back button. This calls finish() on this activity and pops the
+			// back stack.
+			super.onBackPressed();
+		} else {
+			// Otherwise, select the previous step.
+			mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+		}
+	}
+
+	private class SlidePageAdapter extends FragmentPagerAdapter {
+		private List<Fragment> fragments;
+
+		public SlidePageAdapter(FragmentManager fm, List<Fragment> fragments) {
+			super(fm);
+			this.fragments = fragments;
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return this.fragments.get(position);
+		}
+
+		@Override
+		public int getCount() {
+			return this.fragments.size();
+		}
+	}
 }
