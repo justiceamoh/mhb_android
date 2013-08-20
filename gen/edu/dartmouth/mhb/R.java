116a44fb2e167bd3766d9a907952d5edad39a502
Justice Amoh <justiceamoh@gmail.com>
2013-08-20 20:22:53 +0000

    implemented DrawerLayout, untested

--- gen/edu/dartmouth/mhb/R.java
+++ gen/edu/dartmouth/mhb/R.java
@@ -8,86 +8,97 @@
 package edu.dartmouth.mhb;
 
 public final class R {
+    public static final class array {
+        public static final int sidebar_array=0x7f070000;
+    }
     public static final class attr {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
-        public static final int author_size=0x7f060003;
-        public static final int lyrics_size=0x7f060005;
-        public static final int number_size=0x7f060004;
-        public static final int title_size=0x7f060002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int author_size=0x7f050003;
+        public static final int lyrics_size=0x7f050005;
+        public static final int number_size=0x7f050004;
+        public static final int title_size=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_action_attach=0x7f020000;
-        public static final int ic_action_call=0x7f020001;
-        public static final int ic_action_copy=0x7f020002;
-        public static final int ic_action_cut=0x7f020003;
-        public static final int ic_action_delete=0x7f020004;
-        public static final int ic_action_done=0x7f020005;
-        public static final int ic_action_edit=0x7f020006;
-        public static final int ic_action_locate=0x7f020007;
-        public static final int ic_action_mail=0x7f020008;
-        public static final int ic_action_mail_add=0x7f020009;
-        public static final int ic_action_microphone=0x7f02000a;
-        public static final int ic_action_overflow=0x7f02000b;
-        public static final int ic_action_paste=0x7f02000c;
-        public static final int ic_action_photo=0x7f02000d;
-        public static final int ic_action_refresh=0x7f02000e;
-        public static final int ic_action_search=0x7f02000f;
-        public static final int ic_action_select_all=0x7f020010;
-        public static final int ic_action_send=0x7f020011;
-        public static final int ic_action_share=0x7f020012;
-        public static final int ic_action_star=0x7f020013;
-        public static final int ic_action_user=0x7f020014;
-        public static final int ic_action_user_add=0x7f020015;
-        public static final int ic_action_video=0x7f020016;
-        public static final int ic_launcher=0x7f020017;
+        public static final int about=0x7f020000;
+        public static final int canticles=0x7f020001;
+        public static final int creeds=0x7f020002;
+        public static final int drawer_shadow=0x7f020003;
+        public static final int favorites=0x7f020004;
+        public static final int hymns=0x7f020005;
+        public static final int ic_action_attach=0x7f020006;
+        public static final int ic_action_call=0x7f020007;
+        public static final int ic_action_copy=0x7f020008;
+        public static final int ic_action_cut=0x7f020009;
+        public static final int ic_action_delete=0x7f02000a;
+        public static final int ic_action_done=0x7f02000b;
+        public static final int ic_action_edit=0x7f02000c;
+        public static final int ic_action_locate=0x7f02000d;
+        public static final int ic_action_mail=0x7f02000e;
+        public static final int ic_action_mail_add=0x7f02000f;
+        public static final int ic_action_microphone=0x7f020010;
+        public static final int ic_action_overflow=0x7f020011;
+        public static final int ic_action_paste=0x7f020012;
+        public static final int ic_action_photo=0x7f020013;
+        public static final int ic_action_refresh=0x7f020014;
+        public static final int ic_action_search=0x7f020015;
+        public static final int ic_action_select_all=0x7f020016;
+        public static final int ic_action_send=0x7f020017;
+        public static final int ic_action_share=0x7f020018;
+        public static final int ic_action_star=0x7f020019;
+        public static final int ic_action_user=0x7f02001a;
+        public static final int ic_action_user_add=0x7f02001b;
+        public static final int ic_action_video=0x7f02001c;
+        public static final int ic_drawer=0x7f02001d;
+        public static final int ic_launcher=0x7f02001e;
+        public static final int today=0x7f02001f;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000e;
-        public static final int btnLoad=0x7f090000;
-        public static final int inner_content=0x7f090006;
-        public static final int list=0x7f090005;
-        public static final int menu_search=0x7f09000d;
-        public static final int pager=0x7f090007;
-        public static final int sauthor=0x7f09000a;
-        public static final int slidedout_cover=0x7f09000c;
-        public static final int slideout_placeholder=0x7f09000b;
-        public static final int snumber=0x7f090009;
-        public static final int stitle=0x7f090008;
-        public static final int textAuthor=0x7f090003;
-        public static final int textLyrics=0x7f090004;
-        public static final int textNumber=0x7f090001;
-        public static final int textTitle=0x7f090002;
+        public static final int action_settings=0x7f0a000f;
+        public static final int content_frame=0x7f0a0001;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int image=0x7f0a0006;
+        public static final int inner_content=0x7f0a0004;
+        public static final int left_drawer=0x7f0a0002;
+        public static final int list=0x7f0a0003;
+        public static final int menu_search=0x7f0a000e;
+        public static final int pager=0x7f0a0005;
+        public static final int sauthor=0x7f0a000d;
+        public static final int snumber=0x7f0a000c;
+        public static final int stitle=0x7f0a000b;
+        public static final int textAuthor=0x7f0a0009;
+        public static final int textLyrics=0x7f0a000a;
+        public static final int textNumber=0x7f0a0007;
+        public static final int textTitle=0x7f0a0008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_search_view=0x7f030001;
         public static final int activity_slide_page=0x7f030002;
-        public static final int fragment_slide_page=0x7f030003;
-        public static final int hymnresult=0x7f030004;
-        public static final int slideout=0x7f030005;
+        public static final int drawer_item=0x7f030003;
+        public static final int fragment_sidebar=0x7f030004;
+        public static final int fragment_slide_page=0x7f030005;
+        public static final int hymnresult=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_search=0x7f050007;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hymn_text=0x7f050008;
-        public static final int search_hint=0x7f050005;
-        public static final int search_label=0x7f050006;
-        public static final int ui_author_title=0x7f050004;
-        public static final int ui_number_title=0x7f050002;
-        public static final int ui_title_title=0x7f050003;
+        public static final int action_search=0x7f060007;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int drawer_close=0x7f060009;
+        public static final int drawer_open=0x7f060008;
+        public static final int hymn_text=0x7f06000a;
+        public static final int search_hint=0x7f060005;
+        public static final int search_label=0x7f060006;
+        public static final int ui_author_title=0x7f060004;
+        public static final int ui_number_title=0x7f060002;
+        public static final int ui_title_title=0x7f060003;
     }
     public static final class style {
         /** 
@@ -99,23 +110,12 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int searchable=0x7f040000;
--- src/edu/dartmouth/mhb/MainActiivity.java
+++ src/edu/dartmouth/mhb/MainActiivity.java
@@ -2,21 +2,33 @@ package edu.dartmouth.mhb;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.SearchManager;
 import android.content.Context;
-import android.os.Build;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 
 public class MainActiivity extends FragmentActivity {
@@ -24,19 +36,68 @@ public class MainActiivity extends FragmentActivity {
 	MySQLiteHelper myDBHelper;
 	private HymnsDataSource datasource;
 	private List<Hymn> hymns;
-
+	
+	private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;		    
+    private ActionBarDrawerToggle mDrawerToggle;
+	
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mDrawerMenuTitles;
+    
+    
+    
 	private ViewPager mPager;
 	private PagerAdapter mPagerAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-			    
+		super.onCreate(savedInstanceState);	    
 		
 		setContentView(R.layout.activity_slide_page);
-//	    ActionBar actionBar = getActionBar();
-//	    actionBar.setDisplayHomeAsUpEnabled(true);
+
+		mTitle = mDrawerTitle = getTitle();
+		mDrawerMenuTitles = getResources().getStringArray(R.array.sidebar_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);		
 		
+        //TODO work on array adapter for drawer items
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_item, mDrawerMenuTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        
+        
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+       
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+                ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        
+        
+        
+        
+        
 		context = getApplicationContext();
 		datasource = new HymnsDataSource(this);
 		datasource.open();
@@ -94,8 +155,32 @@ public class MainActiivity extends FragmentActivity {
 		return super.onCreateOptionsMenu(menu);
 	}
 
+	//TODO set for all menu items, to hide when drawer opens
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.menu_search).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
 	
-	
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         // The action bar home/up action should open or close the drawer.
+         // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        //TODO Handle action buttons with switch statements
+        return super.onOptionsItemSelected(item);
+
+    }	
+    
+    
+    
+    
+    
 	@Override
 	public void onBackPressed() {
 		if (mPager.getCurrentItem() == 0) {
@@ -109,6 +194,7 @@ public class MainActiivity extends FragmentActivity {
 			mPager.setCurrentItem(mPager.getCurrentItem() - 1);
 		}
 	}
+	
 
 	private class SlidePageAdapter extends FragmentPagerAdapter {
 		private List<Fragment> fragments;
@@ -128,4 +214,90 @@ public class MainActiivity extends FragmentActivity {
 			return this.fragments.size();
 		}
 	}
+	
+	
+		///////////////////////////////////////
+		////  Methods for  DrawerLayout ///////	
+		//////////////////////////////////////	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+	    @Override
+	    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	        selectItem(position);
+	    }
+	}
+
+	/** Swaps fragments in the main content view */
+	private void selectItem(int position) {
+	    // Create a new fragment and specify the planet to show based on position
+	    Fragment fragment = new SidebarMenuFragment();
+	    Bundle args = new Bundle();
+	    args.putInt(SidebarMenuFragment.ARG_PLANET_NUMBER, position);
+	    fragment.setArguments(args);
+
+	    // Insert the fragment by replacing any existing fragment
+	    FragmentManager fragmentManager = getFragmentManager();
+	    fragmentManager.beginTransaction()
+	                   .replace(R.id.content_frame, fragment)
+	                   .commit();
+
+	    // Highlight the selected item, update the title, and close the drawer
+	    mDrawerList.setItemChecked(position, true);
+	    setTitle(mDrawerMenuTitles[position]);
+	    mDrawerLayout.closeDrawer(mDrawerList);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+	    mTitle = title;
+	    getActionBar().setTitle(mTitle);
+	}
+	
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    
+    
+    //TODO Create multiple fragments for each sidebar menu
+    /**
+     * Fragment that appears in the "content_frame", shows a planet
+     */
+    public static class SidebarMenuFragment extends Fragment {
+        public static final String ARG_PLANET_NUMBER = "planet_number";
+
+        public SidebarMenuFragment() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_sidebar, container, false);
+            int i = getArguments().getInt(ARG_PLANET_NUMBER);
+            String sidebar_menu = getResources().getStringArray(R.array.sidebar_array)[i];
+
+            int imageId = getResources().getIdentifier(sidebar_menu.toLowerCase(Locale.getDefault()),
+                            "drawable", getActivity().getPackageName());
+            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+            getActivity().setTitle(sidebar_menu);
+            return rootView;
+        }
+    }
+	
+	
 }
