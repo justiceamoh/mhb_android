9f37246c549c7cff8b9a7940578d43f0e7b83cd7
Justice Amoh <justiceamoh@gmail.com>
2013-08-22 11:41:15 +0000

    implemented drawerlayout, error with android-support-4 though

--- gen/edu/dartmouth/mhb/R.java
+++ gen/edu/dartmouth/mhb/R.java
@@ -8,6 +8,9 @@
 package edu.dartmouth.mhb;
 
 public final class R {
+    public static final class array {
+        public static final int planets_array=0x7f070000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,61 +28,78 @@ public final class R {
         public static final int title_size=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_action_attach=0x7f020000;
-        public static final int ic_action_call=0x7f020001;
-        public static final int ic_action_copy=0x7f020002;
-        public static final int ic_action_cut=0x7f020003;
-        public static final int ic_action_delete=0x7f020004;
-        public static final int ic_action_done=0x7f020005;
-        public static final int ic_action_edit=0x7f020006;
-        public static final int ic_action_locate=0x7f020007;
-        public static final int ic_action_mail=0x7f020008;
-        public static final int ic_action_mail_add=0x7f020009;
-        public static final int ic_action_microphone=0x7f02000a;
-        public static final int ic_action_overflow=0x7f02000b;
-        public static final int ic_action_paste=0x7f02000c;
-        public static final int ic_action_photo=0x7f02000d;
-        public static final int ic_action_refresh=0x7f02000e;
-        public static final int ic_action_search=0x7f02000f;
-        public static final int ic_action_select_all=0x7f020010;
-        public static final int ic_action_send=0x7f020011;
-        public static final int ic_action_share=0x7f020012;
-        public static final int ic_action_star=0x7f020013;
-        public static final int ic_action_user=0x7f020014;
-        public static final int ic_action_user_add=0x7f020015;
-        public static final int ic_action_video=0x7f020016;
-        public static final int ic_launcher=0x7f020017;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int earth=0x7f020001;
+        public static final int ic_action_attach=0x7f020002;
+        public static final int ic_action_call=0x7f020003;
+        public static final int ic_action_copy=0x7f020004;
+        public static final int ic_action_cut=0x7f020005;
+        public static final int ic_action_delete=0x7f020006;
+        public static final int ic_action_done=0x7f020007;
+        public static final int ic_action_edit=0x7f020008;
+        public static final int ic_action_locate=0x7f020009;
+        public static final int ic_action_mail=0x7f02000a;
+        public static final int ic_action_mail_add=0x7f02000b;
+        public static final int ic_action_microphone=0x7f02000c;
+        public static final int ic_action_overflow=0x7f02000d;
+        public static final int ic_action_paste=0x7f02000e;
+        public static final int ic_action_photo=0x7f02000f;
+        public static final int ic_action_refresh=0x7f020010;
+        public static final int ic_action_search=0x7f020011;
+        public static final int ic_action_select_all=0x7f020012;
+        public static final int ic_action_send=0x7f020013;
+        public static final int ic_action_share=0x7f020014;
+        public static final int ic_action_star=0x7f020015;
+        public static final int ic_action_user=0x7f020016;
+        public static final int ic_action_user_add=0x7f020017;
+        public static final int ic_action_video=0x7f020018;
+        public static final int ic_drawer=0x7f020019;
+        public static final int ic_launcher=0x7f02001a;
+        public static final int jupiter=0x7f02001b;
+        public static final int mars=0x7f02001c;
+        public static final int mercury=0x7f02001d;
+        public static final int neptune=0x7f02001e;
+        public static final int saturn=0x7f02001f;
+        public static final int uranus=0x7f020020;
+        public static final int venus=0x7f020021;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
-        public static final int btnLoad=0x7f090000;
-        public static final int inner_content=0x7f090006;
-        public static final int list=0x7f090005;
-        public static final int menu_search=0x7f09000b;
-        public static final int pager=0x7f090007;
-        public static final int sauthor=0x7f09000a;
-        public static final int snumber=0x7f090009;
-        public static final int stitle=0x7f090008;
-        public static final int textAuthor=0x7f090003;
-        public static final int textLyrics=0x7f090004;
-        public static final int textNumber=0x7f090001;
-        public static final int textTitle=0x7f090002;
+        public static final int action_settings=0x7f0a000f;
+        public static final int content_frame=0x7f0a0001;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int image=0x7f0a0006;
+        public static final int inner_content=0x7f0a0004;
+        public static final int left_drawer=0x7f0a0002;
+        public static final int list=0x7f0a0003;
+        public static final int menu_search=0x7f0a000e;
+        public static final int pager=0x7f0a0005;
+        public static final int sauthor=0x7f0a000d;
+        public static final int snumber=0x7f0a000c;
+        public static final int stitle=0x7f0a000b;
+        public static final int textAuthor=0x7f0a0009;
+        public static final int textLyrics=0x7f0a000a;
+        public static final int textNumber=0x7f0a0007;
+        public static final int textTitle=0x7f0a0008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_search_view=0x7f030001;
         public static final int activity_slide_page=0x7f030002;
-        public static final int fragment_slide_page=0x7f030003;
-        public static final int hymnresult=0x7f030004;
+        public static final int drawer_list_item=0x7f030003;
+        public static final int fragment_planet=0x7f030004;
+        public static final int fragment_slide_page=0x7f030005;
+        public static final int hymnresult=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
         public static final int action_search=0x7f060007;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int hymn_text=0x7f060008;
+        public static final int drawer_close=0x7f060009;
+        public static final int drawer_open=0x7f060008;
+        public static final int hymn_text=0x7f06000a;
         public static final int search_hint=0x7f060005;
         public static final int search_label=0x7f060006;
         public static final int ui_author_title=0x7f060004;
@@ -108,11 +128,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int searchable=0x7f040000;
