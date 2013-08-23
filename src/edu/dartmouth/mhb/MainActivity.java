05ae8b0d42884e0790326fc53f5e7841eb2bda18
Justice Amoh <justiceamoh@gmail.com>
2013-08-23 13:57:14 +0000

    removed onBackPressed from MainActivity

--- src/edu/dartmouth/mhb/MainActivity.java
+++ src/edu/dartmouth/mhb/MainActivity.java
@@ -169,19 +169,6 @@ public class MainActivity extends FragmentActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	@Override
-	public void onBackPressed() {
-		if (mPager.getCurrentItem() == 0) {
-			// If the user is currently looking at the first step, allow the
-			// system to handle the
-			// Back button. This calls finish() on this activity and pops the
-			// back stack.
-			super.onBackPressed();
-		} else {
-			// Otherwise, select the previous step.
-			mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-		}
-	}
 
 	// ///////////////////////////
 	// //Drawer Layout Methods////
