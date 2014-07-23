8703ff1ccf3adad7adabba5574bcb41ca3b72660
Justice Amoh <justiceamoh@gmail.com>
2014-07-23 14:09:31 +0000

    apk changes before branch

--- gen/edu/dartmouth/mhb/R.java
+++ gen/edu/dartmouth/mhb/R.java
@@ -15,10 +15,6 @@ public final class R {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
@@ -39,61 +35,65 @@ public final class R {
         public static final int drawer_shadow=0x7f020003;
         public static final int ic_action_back=0x7f020004;
         public static final int ic_action_browse=0x7f020005;
-        public static final int ic_action_done=0x7f020006;
-        public static final int ic_action_format=0x7f020007;
-        public static final int ic_action_overflow=0x7f020008;
-        public static final int ic_action_search=0x7f020009;
-        public static final int ic_drawer=0x7f02000a;
-        public static final int ic_launcher=0x7f02000b;
-        public static final int paper_texture=0x7f02000c;
-        public static final int splash=0x7f02000d;
+        public static final int ic_action_contents=0x7f020006;
+        public static final int ic_action_done=0x7f020007;
+        public static final int ic_action_format=0x7f020008;
+        public static final int ic_action_overflow=0x7f020009;
+        public static final int ic_action_search=0x7f02000a;
+        public static final int ic_drawer=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int paper_texture=0x7f02000d;
+        public static final int splash=0x7f02000e;
     }
     public static final class id {
-        public static final int action_contents=0x7f0a0015;
-        public static final int action_day_night=0x7f0a0017;
-        public static final int action_format=0x7f0a0016;
-        public static final int button_next=0x7f0a0010;
-        public static final int button_prev=0x7f0a000e;
+        public static final int action_contents=0x7f0a0017;
+        public static final int button_next=0x7f0a0013;
+        public static final int button_prev=0x7f0a0011;
         public static final int content_frame=0x7f0a0002;
-        public static final int contents_list=0x7f0a0007;
+        public static final int contents_list=0x7f0a000a;
         public static final int drawer_layout=0x7f0a0001;
-        public static final int gridview=0x7f0a0006;
-        public static final int inner_content=0x7f0a000d;
+        public static final int gridview=0x7f0a0009;
+        public static final int inner_content=0x7f0a0010;
         public static final int left_drawer=0x7f0a0003;
-        public static final int menu_search=0x7f0a0014;
-        public static final int pager=0x7f0a000f;
-        public static final int sauthor=0x7f0a0013;
+        public static final int menu_format=0x7f0a0007;
+        public static final int menu_left_caret=0x7f0a0006;
+        public static final int menu_right_caret=0x7f0a0008;
+        public static final int menu_search=0x7f0a0018;
+        public static final int pager=0x7f0a0012;
+        public static final int sauthor=0x7f0a0016;
         public static final int search_list=0x7f0a0004;
-        public static final int snumber=0x7f0a0012;
+        public static final int snumber=0x7f0a0015;
         public static final int splashscreen=0x7f0a0005;
-        public static final int stitle=0x7f0a0011;
+        public static final int stitle=0x7f0a0014;
         public static final int tab_pager=0x7f0a0000;
-        public static final int textAuthor=0x7f0a000a;
-        public static final int textLyrics=0x7f0a000b;
-        public static final int textNumber=0x7f0a0008;
-        public static final int textTitle=0x7f0a0009;
-        public static final int textView1=0x7f0a000c;
+        public static final int textAuthor=0x7f0a000d;
+        public static final int textLyrics=0x7f0a000e;
+        public static final int textNumber=0x7f0a000b;
+        public static final int textTitle=0x7f0a000c;
+        public static final int textView1=0x7f0a000f;
     }
     public static final class layout {
-        public static final int activity_contents=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_search=0x7f030002;
-        public static final int activity_splashscreen=0x7f030003;
-        public static final int contents_list_item_1=0x7f030004;
-        public static final int drawer_list_item=0x7f030005;
-        public static final int fragment_contents_grid=0x7f030006;
-        public static final int fragment_contents_list=0x7f030007;
-        public static final int fragment_hymn_page=0x7f030008;
-        public static final int fragment_menu_about=0x7f030009;
-        public static final int fragment_menu_canticles=0x7f03000a;
-        public static final int fragment_menu_creeds=0x7f03000b;
-        public static final int fragment_menu_favorites=0x7f03000c;
-        public static final int fragment_menu_hymns=0x7f03000d;
-        public static final int fragment_menu_today=0x7f03000e;
-        public static final int fragment_tab_authors=0x7f03000f;
-        public static final int fragment_tab_numbers=0x7f030010;
-        public static final int fragment_tab_titles=0x7f030011;
-        public static final int hymnresult=0x7f030012;
+        public static final int actionbar=0x7f030000;
+        public static final int activity_contents=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_search=0x7f030003;
+        public static final int activity_splashscreen=0x7f030004;
+        public static final int bottombar=0x7f030005;
+        public static final int contents_list_item_1=0x7f030006;
+        public static final int drawer_list_item=0x7f030007;
+        public static final int fragment_contents_grid=0x7f030008;
+        public static final int fragment_contents_list=0x7f030009;
+        public static final int fragment_hymn_page=0x7f03000a;
+        public static final int fragment_menu_about=0x7f03000b;
+        public static final int fragment_menu_canticles=0x7f03000c;
+        public static final int fragment_menu_creeds=0x7f03000d;
+        public static final int fragment_menu_favorites=0x7f03000e;
+        public static final int fragment_menu_hymns=0x7f03000f;
+        public static final int fragment_menu_today=0x7f030010;
+        public static final int fragment_tab_authors=0x7f030011;
+        public static final int fragment_tab_numbers=0x7f030012;
+        public static final int fragment_tab_titles=0x7f030013;
+        public static final int hymnresult=0x7f030014;
     }
     public static final class menu {
         public static final int main=0x7f090000;
@@ -127,17 +127,6 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
@@ -145,8 +134,7 @@ public final class R {
          */
         public static final int AppTheme=0x7f080001;
         /**  TextView Styles 
- 	   <item name="fontFamily">baroque_script</item>
- 
+ 	   <item name="fontFamily">baroque_script</item> 
          */
         public static final int Baroque=0x7f080008;
         public static final int ContentTheme=0x7f080006;
