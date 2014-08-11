149888a3e68589fbd0a6438a8bec08fd1bd775c5
Justice Amoh <justiceamoh@gmail.com>
2014-08-11 23:04:14 +0000

    implemented alphabetically guided fastscrolling

--- src/edu/dartmouth/mhb/SortedHymnArrayAdapter.java
+++ src/edu/dartmouth/mhb/SortedHymnArrayAdapter.java
@@ -1,18 +1,25 @@
 package edu.dartmouth.mhb;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.SectionIndexer;
 import android.widget.TextView;
 
-public class SortedHymnArrayAdapter extends ArrayAdapter<Hymn> {
+public class SortedHymnArrayAdapter extends ArrayAdapter<Hymn> implements SectionIndexer{
 	private final Context context;
 	private final ArrayList<Hymn> hymns;
 	private final int sortType;
+
+	HashMap<String, Integer> alphaIndexer;
+	String[] sections;
 
 	public SortedHymnArrayAdapter(Context context, ArrayList<Hymn> hymns,
 			int sortType) {
@@ -20,6 +27,34 @@ public class SortedHymnArrayAdapter extends ArrayAdapter<Hymn> {
 		this.context = context;
 		this.hymns = hymns;
 		this.sortType = sortType;
+
+		alphaIndexer = new HashMap<String, Integer>();
+		int size = hymns.size();
+		for (int x = 0; x < size; x++){
+			String ch;
+			switch (sortType) {
+				case Globals.SORT_TITLE:
+				default:
+					ch = hymns.get(x).getTitle();
+					break;
+				case Globals.SORT_AUTHOR:
+					ch = hymns.get(x).getAuthor();
+					break;
+				case Globals.SORT_FIRSTLINE:
+					ch = Utilities.getFirstLine(hymns.get(x)
+							.getLyrics());
+					break;
+			}	
+			ch = ch.substring(0,1);
+			ch = ch.toUpperCase();
+			alphaIndexer.put(ch,x);
+		}
+
+		Set<String> sectionLetters = alphaIndexer.keySet();
+		ArrayList<String> sectionList = new ArrayList<String>(sectionLetters);
+		Collections.sort(sectionList);
+		sections = new String[sectionList.size()];
+		sectionList.toArray(sections);	
 	}
 
 	@Override
@@ -51,6 +86,21 @@ public class SortedHymnArrayAdapter extends ArrayAdapter<Hymn> {
 
 		return rowView;
 
+	}
+
+	@Override
+	public int getPositionForSection(int section) {
+		return alphaIndexer.get(sections[section]);
+	}
+
+	@Override
+	public int getSectionForPosition(int position) {
+		return 1;
+	}
+
+	@Override
+	public Object[] getSections() {
+		return sections;
 	}
 
 }
